8f0c5be58bf68fb2bd32b043310b4f01be137792
Riccardo Galeazzi <riccardor.galeazzi2007@gmail.com>
2026-08-08 23:20:03 +0200

    Add user interaction

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
-const name = "Riccardo";
-const age = 20;
+const readline = require("readline");
 
-console.log(`Ciao ${name}!`);
-console.log(`Ho ${age} anni.`);
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question("Come ti chiami? ", (name) => {
+    rl.question("Quanti anni hai? ", (age) => {
+        rl.question("Quale corso di laurea frequenti? ", (faculty) => {
+            console.log(`Ciao ${name}!`);
+            console.log(`Studi ${faculty}! Benvenuto nel laboratorio software`);
+            console.log(`Tu hai ${age}`);
+            rl.close();
+        });
+    
+    });
+    
+});
